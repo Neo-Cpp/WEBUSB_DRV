7bcc8030fc2167b1adb23ac353f560f6cf56875c
Jerry-Liuu <zw896940553@163.com>
2021-02-24 22:03:33 +0800

    stm32f103c8t6 chip is work to tinyusb demo webusb serial

---
+++ trunk/web103c8t6/MDK-ARM/RTE/_070/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '070' 
+ * Target:  '070' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
